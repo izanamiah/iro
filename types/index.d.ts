2ac6354d916c110b3c7aec4c725c910411fc0a23
izanamiah <jeremiahliu@live.com>
2023-08-08 23:13:23 -0400

    added home page and footer

---
+++ types/index.d.ts
@@ -0,0 +1,25 @@
+import type { Icon } from "lucide-react";
+import { Icons } from "@/components/icons";
+
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    twitter: string;
+    github: string;
+  };
+};
+
+export type NavItem = {
+  title: string;
+  href: string;
+  disabled?: boolean;
+};
+
+export type MainNavItem = NavItem;
+
+export type HomeConfig = {
+  mainNav: MainNavItem[];
+};
